32aaab34fbb5b5d8cab6db56ce4076a71e26f7d3
Chris Konstad <chriskon149@gmail.com>
2016-02-20 13:46:44 -0800

    Added packetizing of data
    We can now send and receive byte arrays longer than the maximum packet
    data size.  ACKs are sent in order, as well as a FIN to end the stream
    and a FINACK to acknowledge.
    
    NOTE: The packets are sent in order, there is no window size or anything
    like that (along with no timeouts and no resending).

--- lib/rdtp.c
+++ lib/rdtp.c
@@ -7,6 +7,9 @@
 
 #include "packet.h"
 
+/**
+ * Send a singular packet of any type
+ */
 void sendPacket(Packet *p, int sockfd, const struct sockaddr *destAddr,
                 socklen_t destLen) {
   // Print for debugging
@@ -26,6 +29,9 @@ void sendPacket(Packet *p, int sockfd, const struct sockaddr *destAddr,
   assert(-1 != bytesSent);  // TODO replace with proper error handling later
 }
 
+/**
+ * Receive a singular packet of any type
+ */
 Packet receivePacket(int sockfd, struct sockaddr *fromAddress,
                      socklen_t *fromAddressLen) {
   uint8_t buffer[MAX_PACKET_SIZE];
@@ -44,39 +50,105 @@ Packet receivePacket(int sockfd, struct sockaddr *fromAddress,
   return ret;
 }
 
+/**
+ * Receive a byte array
+ */
 Buffer receiveBytes(int sockfd, struct sockaddr *fromAddress,
                     socklen_t *fromAddressLen) {
-  // TODO for now, assume one packet is sent. Work on multiple packets later!
   // TODO also check packet flags to ensure it's a TRN packet
-  Packet rec = receivePacket(sockfd, fromAddress, fromAddressLen);
-
-  // Send ACK
-  Packet ack = makeAck(rec.seq);
-  sendPacket(&ack, sockfd, fromAddress, *fromAddressLen);
-
-  // Turn received packets' data into a single buffer, and return that
   Buffer recBytes;
-  recBytes.data = (uint8_t *)malloc(rec.length * sizeof(uint8_t));
-  assert(recBytes.data);
-  recBytes.length = rec.length;
-  memcpy(recBytes.data, rec.data, rec.length);
+  recBytes.data = NULL;
+  recBytes.length = 0;
+
+  while (1) {
+    Packet rec = receivePacket(sockfd, fromAddress, fromAddressLen);
+
+    // Handle different packet types
+    if (!rec.isAck && !rec.isFin) {
+      // Copy packet data into recBytes
+      recBytes.data =
+          (uint8_t *)realloc(recBytes.data, recBytes.length + rec.length);
+      assert(recBytes.data);
+      memcpy(&recBytes.data[recBytes.length], rec.data, rec.length);
+      recBytes.length += rec.length;
+
+      // Send ACK
+      Packet ack = makeAck(rec.seq);
+      sendPacket(&ack, sockfd, fromAddress, *fromAddressLen);
+    } else if (!rec.isAck && rec.isFin) {
+      // Send FINACK
+      Packet finAck = makeFinAck();
+      sendPacket(&finAck, sockfd, fromAddress, *fromAddressLen);
+      break;
+      // TODO Handle rest of the ending handshake
+    }
+    // TODO Handle rest of the packet types
+  }
 
   return recBytes;
 }
 
+/**
+ * Calculate how hany packets are required to send a buffer
+ */
+int numPacketsRequired(Buffer buf) {
+  int base = buf.length / MAX_PACKET_DATA;
+  int mod = buf.length % MAX_PACKET_DATA;
+  if (mod) {
+    base++;
+  }
+  return base;
+}
+
+/**
+ * Create an array of TRN packets that point into buf
+ * The caller should NOT free the packets, as they point into buf.
+ * The caller should free the packet list, though.
+ */
+int packetize(Buffer buf, Packet **packets) {
+  int numPackets = numPacketsRequired(buf);
+  *packets = (Packet *)malloc(numPackets * sizeof(Packet));
+  assert(0 != *packets);
+  size_t dataOffset = 0;
+  for (int i = 0; i < numPackets; i++) {
+    (*packets)[i] = makeTrn(dataOffset);
+    (*packets)[i].data = &buf.data[dataOffset];
+    if (i < (numPackets - 1)) {
+      (*packets)[i].length = MAX_PACKET_DATA;
+    } else {
+      (*packets)[i].length = buf.length - dataOffset;
+    }
+    dataOffset += (*packets)[i].length;
+  }
+
+  return numPackets;
+}
+
+/**
+ * Send a byte array
+ */
 void sendBytes(Buffer buf, int sockfd, const struct sockaddr *destAddr,
                socklen_t destLen) {
-  // TODO for now, assume buf fits into one packet. Packetize it later!
-  assert(buf.length <= MAX_PACKET_SIZE);
+  // Packetize the data
+  Packet *packets;
+  int numPackets = packetize(buf, &packets);
 
-  // TODO Add proper error handling, resending, etc.
-  Packet packet = makeTrn(118);  // change seq number
-  packet.data = buf.data;
-  packet.length = buf.length;
+  for (int i = 0; i < numPackets; i++) {
+    // TODO Add proper error handling, resending, etc.
+    // Send packet
+    sendPacket(&packets[i], sockfd, destAddr, destLen);
 
-  sendPacket(&packet, sockfd, destAddr, destLen);
+    // Handle ACK
+    Packet rec = receivePacket(sockfd, NULL, NULL);
+    assert(rec.isAck);  // TODO replace with real error handling
+  }
 
-  // TODO Handle ACK
-  Packet rec = receivePacket(sockfd, NULL, NULL);
-  assert(rec.isAck);
+  // Send FIN
+  Packet fin = makeFin();
+  sendPacket(&fin, sockfd, destAddr, destLen);
+
+  // Handle FINACK
+  Packet finAck = receivePacket(sockfd, NULL, NULL);
+  assert(finAck.isAck);  // TODO replace with real error handling
+  assert(finAck.isFin);  // TODO replace with real error handling
 }
