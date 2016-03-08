1616a1be7ce590ba3bbd19149a90d7f2bd7b7e4e
Chris Konstad <chriskon149@gmail.com>
2016-03-08 15:06:57 -0800

    Started sequence number rollovers

--- lib/packet.c
+++ lib/packet.c
@@ -5,9 +5,10 @@
  * The helper functions can be used to send and receive packets as byte arrays.
  */
 
-const int MAX_PACKET_SIZE = 1000;    // number of bytes
+const int MAX_PACKET_SIZE = 50;    // number of bytes
 const int PACKET_HEADER_LENGTH = 5;  // number of bytes
 const int MAX_PACKET_DATA = MAX_PACKET_SIZE - PACKET_HEADER_LENGTH;
+const int MAX_SEQ_NUM = 300;  // 30,000 is the max seq num allowed
 
 const int FLAG_ACK = 1 << 7;
 const int FLAG_FIN = 1 << 6;
